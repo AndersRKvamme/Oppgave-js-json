4a68b15e2734bc98445f93e5f5efe8cd7d456414
AndersRKvamme <rka092024@gmail.com>
2024-11-28 11:08:32 +0100

    lø

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const PersonArray = [
     {
         FirstName: "Ola", 
         Surname: "Nordmann",
-        Age: 18,
+        Age: 12,
         job: false,
     },
     {
@@ -20,16 +20,16 @@ const PersonArray = [
         job: true,
     },
     {
-        FirstName: "Erna", 
-        Surname: "Solberg",
-        Age: 80,
+        FirstName: "Todd", 
+        Surname: "Howard",
+        Age: 69,
         job: true,
     }
-]
+];
 //2. print First and last name of the first person in the array. using dot notation on firstname and bracket notation last name
 //------------------------------------------------------------------------------------------------------------------------------------------------
 
-console.log(PersonArray.FirstName[0]);
+console.log(PersonArray[0].FirstName + " " + [PersonArray[0].Surname]);
 
 
 
@@ -37,18 +37,58 @@ console.log(PersonArray.FirstName[0]);
 //create a method in every person object that returns first and last name, call it fullName. This can be done manually for each one or with a loop.
 //Print fullName of the second person in the array by calling the method.
 //------------------------------------------------------------------------------------------------------------------------------------------------
+// const fullName = PersonArray.Firstname + PersonArray.Surname;
+// console.log(fullName[1]);
+for (let i = 0; i < PersonArray.length ; i++) {
+    PersonArray[i].fullName = function () {
+      return this.FirstName + " " + this.Surname;
+    };
+  }
+  
+  console.log(PersonArray[2].fullName());
+
 
 //4. Its the third person's birthday! And their job status changed. Update their age and job status using dot notation.
 //------------------------------------------------------------------------------------------------------------------------------------------------
+
+// PersonArray[2].Age = 91;
+// PersonArray[2].Job = false;
+// console.table(PersonArray[2]);
+
 
 //5. Person three is throwing a giant party! create a function called fotballPubben(). The function should check if the person is over 18, print "party time" if they are and "too young" if they are not. It should also print the name of the person.
 // use a loop to call the function for every person in the array.
 //------------------------------------------------------------------------------------------------------------------------------------------------
 
+// function fotballPubben(person) {
+//     if (person.Age>=18){
+//         console.log(person.FirstName + " Party time!")
+//     }
+//      else {
+//         console.log(person.FirstName + " Too young.");
+//          }
+//     }
+// PersonArray.forEach((person)=>{
+//     fotballPubben(person)
+// })
+
+
+
 //6. It's time for school! Create a function called university. It should take in an person object as well as type of degree (bachelors or masters) as arguments.
 // The function should update age and add two properties called "degree" and "student loan". The value of age, degree and student loan should change depending on what degree
 //was passed into the function. Send one person to uni and print the result.
 //------------------------------------------------------------------------------------------------------------------------------------------------
+
+function university (person, b){
+    if (b === "bachelors") {
+        person.Age += 2
+        person.degree = "bachelors"
+    }
+}
+
+university(person)
+
+console.table(PersonArray);
 
 // 7. API TIME!
 // Read the documentation of this dog API: https://dog.ceo/dog-api/documentation/
